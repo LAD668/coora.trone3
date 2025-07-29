33b6c5d5e61b52db29d272ba670a5b75d50eac53
COORA <loanihateyou@gmail.com>
2025-07-29 11:59:50 +0200

    Fix React.use error by adding explicit React imports

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,5 @@
+@@ .. @@
+-import React from 'react';
+ import { Tabs } from 'expo-router';
++import React from 'react';
+ import { MessageSquare, Calendar, Users, ChartBar as BarChart3, Zap } from 'lucide-react-native';
